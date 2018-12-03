7696a78fb43d3b78804b70842fe3803e68dd8846
Ron Herrema <rherrema@gmail.com>
2018-12-03 11:43:45 +0000

    Initial commit.

--- DebugExample/ViewController.swift
+++ DebugExample/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var foo = 1.0
+    var fum = 23
+    
+    var myArray = [23, 45, 56]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(foo + fum)
+        print(myArray[3])
     }
 
 
+    @IBAction func subtractNumbers(_ sender: UIButton) {
+    }
+    
+    
+    
 }
 
