1dcb53af95cf442e2899c5ec4a09d069180b79e4
Ron Herrema <rherrema@gmail.com>
2018-12-03 11:34:57 +0000

    Initial Commit

---
+++ DebugExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DebugExample
+//
+//  Created by R J Herrema on 03/12/2018.
+//  Copyright © 2018 R J Herrema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
